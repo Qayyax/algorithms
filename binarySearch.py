f7102937cc551fc23946a40c4047813170c68f1a
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-03-15 14:18:51 -0400

    added binary_search

---
+++ binarySearch.py
@@ -0,0 +1,14 @@
+# Binary search
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if target == arr[mid]:
+            return mid
+        elif target > arr[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return None
